b182fc147ee20285d4ebabe77bd05dec8905f5c9
Jason Lundberg <jason@jrlundberg.com>
2013-03-17 16:13:35 -0600

    Add sendgrid for email support

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -66,6 +66,6 @@ Omrails::Application.configure do
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
 
     # For Devise gem
-  config.action_mailer.default_url_options = { :host => 'localhost:3000' }
+  config.action_mailer.default_url_options = { :host => 'jrlundberg.com' }
   
 end
