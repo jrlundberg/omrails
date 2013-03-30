09e05db4fcd0bbe338023ca92d0418ac3f89c8e7
Jason Lundberg <jason@jrlundberg.com>
2013-03-29 21:25:36 -0600

    Update db migration for pins

--- db/migrate/20130319034336_add_user_id_to_pins.rb
+++ db/migrate/20130319034336_add_user_id_to_pins.rb
@@ -1,6 +1,9 @@
 class AddUserIdToPins < ActiveRecord::Migration
   def change
-    add_column :pins, :user_id, :integer
+    create_table :pins do |t|
+      t.integer :user_id
+      t.timestamps
+    end
     add_index :pins, :user_id
   end
 end
